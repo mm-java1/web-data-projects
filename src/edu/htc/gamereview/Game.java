c284411e3d1d8fbb66285e8b448ebc05a0b97b2a
BCP27 <gamemeister27@gmail.com>
2016-04-25 19:54:08 -0500

    saving mid class

---
+++ src/edu/htc/gamereview/Game.java
@@ -0,0 +1,105 @@
+package edu.htc.gamereview;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Student on 4/25/2016.
+ */
+public class Game {
+
+    private String name;
+    private String yearReleased;
+    private String platform;
+    private ArrayList<String> tags;
+    private float averageRating;
+    private ArrayList<Review> reviews;
+
+    public ArrayList<Review> getReviews() {
+        return reviews;
+    }
+
+    protected void setReviews(ArrayList<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void addReview(Review review)
+    {
+        if (this.reviews == null)
+        {
+            this.reviews = new ArrayList<Review>();
+        }
+
+    }
+    public Game(){
+        reviews = new ArrayList<Review>();
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) throws InvalidDataTypeException {
+        if (platform == null || platform.trim().isEmpty())
+        {
+            throw new InvalidDataTypeException("The platform has to exist dude, type something");
+        }
+        else
+        {
+            this.platform = platform;
+        }
+    }
+
+    public String getYearReleased() {
+        return yearReleased;
+    }
+
+    public void setYearReleased(String yearReleased) throws InvalidDataTypeException {
+        if (yearReleased == null || yearReleased.trim().isEmpty())
+        {
+            throw new InvalidDataTypeException("The year released cannot be null or empty.");
+        }
+        else if (yearReleased.trim().length() != 4)
+        {
+            throw new InvalidDataTypeException("This ain't the future, nor the past, try again.");
+        }
+        else {
+            try {
+                Integer.parseInt(yearReleased);
+            } catch (NumberFormatException e) {
+                throw new InvalidDataTypeException("The year released must be four digits.");
+            }
+            this.yearReleased = yearReleased;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws InvalidDataTypeException {
+        if (name == null || name.trim().isEmpty())
+        {
+            throw new InvalidDataTypeException("The game name cannot be null or empty");
+        }
+        else
+        {
+            this.name = name;
+        }
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public float getAverageRating() {
+        return averageRating;
+    }
+    // this should be set by the database, not by the user interface
+    protected void setAverageRating(float averageRating) {
+        this.averageRating = averageRating;
+    }
+}
---
+++ src/edu/htc/gamereview/InvalidDataTypeException.java
@@ -0,0 +1,12 @@
+package edu.htc.gamereview;
+
+/**
+ * Created by Student on 4/25/2016.
+ */
+public class InvalidDataTypeException extends Exception{
+
+    public InvalidDataTypeException(String message)
+    {
+        super(message);
+    }
+}
